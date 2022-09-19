a8499f33a1117b6cdc1c174716b011393b030b43
G Adarsh Sonu <61682222+gadarsh043@users.noreply.github.com>
2022-09-20 02:45:29 +0530

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -84,7 +84,7 @@
   }
   .ip {
     background: #e4ecf9;
-    width: 400px;
+    width: 40%;
     height: 280px;
     border-radius: 12px;
     box-shadow: 1px 1px 5px 0 rgb(26 26 67 / 5%), 39px 62.5px 125px -25px rgb(50 50 93 / 20%), 23.4px 37.5px 75px -37.5px rgb(0 0 0 / 40%);
